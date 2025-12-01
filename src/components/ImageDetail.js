57a3f680eee668b525bcb0f5b7caf26a6c9212df
AnurajGhosh <anurajghosh@gmail.com>
2025-12-01 06:36:49 +0530

    added socre parameter from parent to child

--- src/components/ImageDetail.js
+++ src/components/ImageDetail.js
@@ -1,11 +1,12 @@
 import { StyleSheet, Text, View, Image } from 'react-native'
 import React from 'react'
 
-const ImageDetail = ({ title, imageSource }) => {
+const ImageDetail = ({ title, imageSource, score }) => {
   return (
     <View>
       <Image style={styles.image} source={imageSource} />
       <Text style={styles.text}>{title}</Text>
+      <Text style={styles.text2}>Image Score - {score}</Text>
     </View>
   );
 };
@@ -16,13 +17,30 @@ export default ImageDetail
 const styles = StyleSheet.create({
   text: {
     fontSize: 18,
-    color: 'red',
+    color: 'blue',
     fontWeight: 'bold',
-    margin: 30,
+    margin: 5,
+    marginLeft: 10,
   },
+  text2: {
+    fontSize: 18,
+    color: 'green',
+    fontWeight: 'bold',
+    margin: 5,
+    marginLeft: 10,
+  },
+
   image: {
     height: 200,
     width: 200,
     margin: 5,
+    borderRadius: 20,
+    shadowColor: '#000',
+    shadowOffset: {
+      width: 0,
+      height: 2,
+    },
+    shadowOpacity: 0.25,
+    shadowRadius: 3.84,
   },
 });
